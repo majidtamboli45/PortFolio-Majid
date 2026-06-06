0ef10c450c9473670bf410ae62831f4b10e45489
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-06-06 21:58:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed("#typing", {
+
+strings: [
+"AI & Machine Learning Developer",
+"Data Science Enthusiast",
+"Python Developer"
+],
+
+typeSpeed:60,
+backSpeed:40,
+loop:true,
+showCursor:false
+
+});
